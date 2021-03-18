83c5405a12a5de343ccc54e0b00c99e5fe147b56
Lo Duca <conradodld@gmail.com>
2021-03-17 21:04:30 -0300

    Mudança de bank.account para bankAccount

--- scripts/sql/credit.sql
+++ scripts/sql/credit.sql
@@ -2,5 +2,5 @@ CREATE TABLE creditThrowing (
   cc int PRIMARY KEY,
   invoicePostings DOUBLE,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (cc) REFERENCES bank.account(cc)
+  FOREIGN KEY (cc) REFERENCES bankAccount(cc)
   );
--- scripts/sql/debit.sql
+++ scripts/sql/debit.sql
@@ -2,5 +2,5 @@ CREATE TABLE debitThrowing (
   cc int PRIMARY KEY,
   extractRelease DOUBLE,
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (cc) REFERENCES bank.account(cc)
+  FOREIGN KEY (cc) REFERENCES bankAccount(cc)
   );
